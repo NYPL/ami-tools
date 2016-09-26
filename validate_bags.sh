05fcf934383289eef6559334b013134728e72daf
nkrabben <n@nkrabben.com>
2016-09-26 16:54:11 -0400

    add print outs to watch the process

--- validate_bags.sh
+++ validate_bags.sh
@@ -3,10 +3,16 @@
 # 1. Discover directories names
 # 2. Validate directories as bags
 # 3. Log results
-# 4. Email
 
 dateCreated=$(date "+%Y%m%d")
+bags=$(ls -d */)
+i=0
 
-for line in $(ls -d */); do
+for line in $bags; do
+  echo $(date "+%H:%M:%s")": validating" $line
   bagit.py --validate $line 2>> validate_${dateCreated}.log
+  ((i++))
+  [ "$(($i % 10))" -eq 0 ] && echo $i "bags validated"
 done
+
+echo $i "bags validated"
