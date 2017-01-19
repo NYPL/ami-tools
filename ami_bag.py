03781220cc507ae46e16dcb6b5ec3fe559846444
nkrabben <n@nkrabben.com>
2017-01-19 17:08:11 -0500

    move excel and bag classes to their own modules, create module for constants

---
+++ ami_bag.py
@@ -0,0 +1,222 @@
+import os
+import csv
+import argparse
+import re
+import bagit
+import jsonschema
+from ami_excel import ami_excel
+import fix_baginfo
+import logging
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+class ami_bagValidationError(Exception):
+    def __init__(self, message):
+        self.message = message
+    def __str__(self):
+        return repr(self.message)
+
+class ami_bag(bagit.Bag):
+
+    def __init__(self, *args, **kwargs):
+        super(ami_bag, self).__init__(*args, **kwargs)
+        self.data_files = self.payload_entries().keys()
+        self.data_dirs = set([os.path.split(path)[0][5:] for path in self.data_files])
+
+
+    def validate_amibag(self, fast = True, metadata = False):
+        '''
+        run each of the validation checks against an AMI Bag
+        '''
+
+        valid = True
+        try:
+            self.validate(fast = fast, complete = fast)
+        except bagit.BagValidationError as e:
+            LOGGER.error("Error in bag: {0}".format(e.message))
+            valid = False
+
+        try:
+            self.check_filenames()
+        except ami_bagValidationError as e:
+            LOGGER.error("Error in filenames: {0}".format(e.message))
+            valid = False
+
+        try:
+            self.check_directory_depth()
+        except ami_bagValidationError as e:
+            LOGGER.error("Error in path names: {0}".format(e.message))
+            valid = False
+
+        try:
+            self.check_type()
+        except ami_bagValidationError as e:
+            LOGGER.error("Error in AMI bag type: {0}".format(e.message))
+            valid = False
+
+        if self.type == "excel":
+            try:
+                self.check_structure_excelbag()
+            except ami_bagValidationError as e:
+                LOGGER.error("Error in bag structure: {0}".format(e.message))
+                valid = False
+
+            if metadata:
+                try:
+                    self.check_metadata_excel()
+                except ami_bagValidationError as e:
+                    LOGGER.error("Error in bag metadata: {0}".format(e.message))
+                    valid = False
+
+        elif self.type == "json":
+            try:
+                self.check_structure_jsonbag()
+            except ami_bagValidationError as e:
+                LOGGER.error("Error in AMI bag type: {0}".format(e.message))
+                valid = False
+
+        return valid
+
+
+    def check_filenames(self):
+        bad_filenames = []
+
+        for filename in self.data_files:
+            if not re.search(r"[\w]+\.\w+$", os.path.split(filename)[1]):
+                bad_filenames.append(filename)
+
+        if bad_filenames:
+            self.raise_bagerror("Illegal characters in the following filenames - {}".format(bad_filenames))
+
+        return True
+
+    def check_directory_depth(self):
+        bad_dirs = []
+
+        for dir_path in self.data_dirs:
+            if re.search(r"/", dir_path):
+                bad_dirs.append(dir_path)
+
+        if bad_dirs:
+            self.raise_bagerror("Too many levels of directories in data/ - {}".format(bad_dirs))
+
+        return True
+
+
+    def check_type(self):
+        self.type = None
+
+        if "Metadata" in self.data_dirs:
+            self.type = "excel"
+        if any(re.search(r"(PreservationMasters|EditMasters|ServiceCopies)/\w+\.json$", filename) for filename in self.data_files):
+            self.type = "json"
+
+        if not self.type:
+            self.raise_bagerror("Bag is not an Excel bag or JSON bag")
+
+        self.data_exts = set([os.path.splitext(filename)[1].lower() for filename in self.data_files])
+        self.subtype = None
+
+        return True
+
+
+    def compare_content(self, expected_exts):
+        if not expected_exts >= self.data_exts:
+            return False
+        return True
+
+
+    def compare_structure(self, expected_dirs):
+        if not expected_dirs >= self.data_dirs:
+            return False
+        return True
+
+
+    def check_structure_excelbag(self):
+
+        expected_dirs = set(["Metadata", "PreservationMasters", "EditMasters", "ArchiveOriginals", "ProjectFiles"])
+        if not self.compare_structure(expected_dirs):
+            self.raise_bagerror("Excel bags may only have the following directories\nFound: {0}\nExpected: {1}".format(self.data_dirs, expected_dirs))
+
+        if (self.compare_structure(set(["Metadata", "PreservationMasters"])) and
+            self.compare_content(set([".mov", ".xlsx"]))):
+            self.subtype = "video"
+        elif (self.compare_structure(set(["Metadata", "PreservationMasters"])) and
+              self.compare_content(set([".iso", ".xlsx"]))):
+            self.subtype = "dvd"
+        elif (self.compare_structure(set(["Metadata", "PreservationMasters", "EditMasters"])) and
+              self.compare_content(set([".wav", ".xlsx"]))):
+            self.subtype = "audio"
+        elif (self.compare_structure(set(["Metadata", "PreservationMasters"])) and
+              self.compare_content(set([".wav", ".xlsx"]))):
+            self.subtype = "audio w/o edit masters"
+        elif (self.compare_structure(set(["Metadata", "ArchiveOriginals", "PreservationMasters", "EditMasters", "ProjectFiles"])) and
+              self.compare_content(set([".tar", ".mov", ".xlsx", ".fcp", ".prproj"]))):
+            self.subtype = "born-digital video"
+        elif (self.compare_structure(set(["Metadata", "ArchiveOriginals", "EditMasters"])) and
+              self.compare_content(set([".wav", ".xlsx"]))):
+            self.subtype = "born-digital audio"
+
+        if not self.subtype:
+            self.raise_bagerror("Bag does not match an existing profile for Excel bags\nExtensions: {0}\nDirectories: {1}".format(self.data_exts, self.data_dirs))
+
+        return True
+
+
+    def check_structure_jsonbag(self):
+        expected_dirs = set(["PreservationMasters", "ServiceCopies", "EditMasters", "ArchiveOriginals"])
+
+        if not self.compare_structure(expected_dirs):
+            self.raise_bagerror("JSON bags may only have the following directories - {}".format(expected_dirs))
+        if (self.compare_structure(set(["Metadata", "PreservationMasters", "ServiceCopies"])) and
+            self.compare_content(set([".mov", ".json", ".mp4", ".jpeg"]))):
+            self.subtype = "video"
+        if (self.compare_structure(set(["Metadata", "PreservationMasters", "EditMasters"])) and
+            self.compare_content(set([".wav", ".json", ".jpeg"]))):
+            self.subtype = "video"
+
+        return True
+
+
+    def check_metadata_excel(self):
+        self.metadata_files = [filename for filename in self.data_files if os.path.splitext(filename)[1] == ".xlsx"]
+
+        if not self.metadata_files:
+            self.raise_bagerror("Excel bag does not contain any files with xlsx extension")
+
+        bad_excel = []
+
+        for filename in self.metadata_files:
+            excel = ami_excel(os.path.join(self.path, filename))
+            if not excel.validate_workbook():
+                bad_excel.append(filename)
+
+        if bad_excel:
+            self.raise_bagerror("Excel files contain formatting errors")
+
+        return True
+
+
+    def raise_bagerror(self, msg):
+        '''
+        lazy error reporting
+        '''
+
+        raise ami_bagValidationError(msg)
+        logging.error(msg + '\n')
+        return False
+
+
+
+def _configure_logging(opts):
+    log_format = "%(asctime)s - %(levelname)s - %(message)s"
+    if opts.quiet:
+        level = logging.WARNING
+    else:
+        level = logging.INFO
+    if opts.log:
+        logging.basicConfig(filename=opts.log, level=level, format=log_format)
+    else:
+        logging.basicConfig(level=level, format=log_format)
---
+++ ami_excel.py
@@ -0,0 +1,572 @@
+import os, re, datetime
+# handling excel
+import xlrd
+from xlrd import xldate
+from openpyxl import load_workbook
+# data manipulation and output
+import pandas as pd
+import numpy as np
+import csv, json
+
+import ami_md_constants
+
+
+class AMIExcelError(Exception):
+  def __init__(self, value):
+    self.value = value
+  def __str__(self):
+    return repr(self.value)
+
+
+class ami_excel:
+  def __init__(self, filename=None):
+    """
+    Initialize object as excel workbook
+    """
+    self.name = filename
+    self.wb = None
+    if os.path.exists(filename):
+      try:
+        self.wb = xlrd.open_workbook(filename)
+      except:
+        print("not an excel file")
+      else:
+        self.set_amiSheetNames()
+        self.filename = os.path.splitext(os.path.abspath(filename))[0]
+    else:
+      print("not a file")
+
+
+  def validate_workbook(self):
+    """
+    Check the preservation sheet against expectations of Media Ingest.
+    """
+
+    valid = True
+
+    #Check for a sheet that should have preservation metadata data
+    try:
+      self.check_presSheetExists()
+    except AMIExcelError as e:
+      print("Error in workbook sheets: ", e.value)
+      valid = False
+
+    #Check that preservation sheet contains required headers
+    for i in range(0, 3):
+      try:
+        expected = set([item[i] for item in ami_md_constants.MEDIAINGEST_EXPECTED_HEADERS if item[i]])
+        found = self.get_headerRow(self.pres_sheetname, i)
+        self.check_headerRow(expected, found)
+      except AMIExcelError as e:
+        print("Error in preservation header row {}: {}"
+          .format(i + 1, e.value))
+        valid = False
+
+    #Check that preservation sheet headers have the correct heirarchy
+    try:
+      header_entries = set(self.get_headerEntries(self.pres_sheetname))
+      self.check_headerEntries(set(ami_md_constants.MEDIAINGEST_EXPECTED_HEADERS), header_entries)
+    except AMIExcelError as e:
+      print("Error in header entries: ", e.value)
+      valid = False
+
+    #Check that the preservation sheet does not contain equations
+    try:
+      self.check_noequations(self.pres_sheetname)
+    except AMIExcelError as e:
+      print("Error in cell values: ", e.value)
+      valid = False
+
+    return valid
+
+
+  def set_amiSheetNames(self):
+    """
+    Identifies sheets that should contain data about preservation files,
+    edit master files, and untransferred objects.
+    """
+
+    self.pres_sheetname = None
+    self.edit_sheetname = None
+    self.notransfer_sheetname = None
+
+    for sheet in self.wb.sheet_names():
+      sheet_lower = sheet.lower()
+      #Check if two sheets get identfied by regex below?
+      if re.match("(original|preservation|file|full|archive)",
+        sheet_lower):
+        self.pres_sheetname = sheet
+      elif re.match("edit", sheet_lower):
+        self.edit_sheetname = sheet
+      elif re.match("not transferred", sheet_lower):
+        self.notransfer_sheetname = sheet
+
+
+
+  def check_presSheetExists(self):
+    """
+    Checks if a preservation sheet has been identified by
+    set_amiSheetNames()
+    """
+
+    if not self.pres_sheetname:
+      self.raise_excelerror("Required sheet for preservation files" +
+        "could not be found in workbook.")
+
+    return True
+
+
+  def get_headerRow(self, sheetname, row):
+    """
+    Return normalized values from a single row of headers on a
+    specified sheet. Newline characters are retained.
+
+    Keyword arguments:
+    sheetname -- name of the sheet to extract from (self.pres_sheetname
+    or self.edit_sheetname)
+    row -- index of the row to extract from (0-2)
+    """
+
+    sheet = self.wb.sheet_by_name(sheetname)
+    headers = []
+
+    for i in range(0, sheet.ncols):
+      value = sheet.cell(row, i).value
+
+      if value:
+        headers.append(value.lower())
+
+    return headers
+
+
+  def get_headerEntries(self, sheetname):
+    """
+    Convenience method to return all header tuples.
+
+    Keyword arguments:
+    sheetname -- name of the sheet to extract from (self.pres_sheetname
+    or self.edit_sheetname)
+    """
+
+    sheet = self.wb.sheet_by_name(sheetname)
+    header_entries = []
+
+    for i in range(0, sheet.ncols):
+      header_entries.append(self.get_headerEntryAsTuple(sheetname, i))
+
+    return header_entries
+
+
+  def get_headerEntryAsTuple(self, sheetname, column):
+    """
+    Returns tuple of header archiving by stepping backwards from a
+    3rd-level header.
+
+    Keyword arguments:
+    sheetname -- name of the sheet to extract from (self.pres_sheetname
+    or self.edit_sheetname)
+    column -- index of the column of the 3rd-level header
+    """
+
+    sheet = self.wb.sheet_by_name(sheetname)
+
+    key1, key2, key3 = None, None, None
+
+    key3 = sheet.cell(2, column).value
+
+    j = column
+    key2 = sheet.cell(1, j).value
+    while not key2:
+      j -= 1
+      if j == -1:
+        key2 = ""
+        j = column
+        break
+      key2 = sheet.cell(1, j).value
+    k = column
+    key1 = sheet.cell(0, k).value
+    while not key1:
+      k -= 1
+      key1 = sheet.cell(0, k).value
+
+    entry = (str(key1).lower(), str(key2).lower(), str(key3).lower())
+
+    #Create tuple filename in problematic templates
+    if (column == 0 and (not key3 or key1 == '\xa0')):
+      entry = ("reference filename (automatic)", None, None)
+
+    return entry
+
+
+  def get_headerEntryAsString(self, sheetname, column, delimiter = "|"):
+    """
+    Returns delimited string based on the output of
+    get_headerEntryAsTuple()
+
+    Keyword arguments:
+    sheetname -- name of the sheet to extract from (self.pres_sheetname
+    or self.edit_sheetname)
+    column -- index of the column of the 3rd-level header
+    delimiter -- character to separate tuple entries
+    """
+
+    header_entry = self.get_headerEntryAsTuple(sheetname, column)
+    #remove empty tuple values before adding delimiter
+    header_string = delimiter.join(filter(None, header_entry))
+    #remove newlines since they're inconsistent
+    header_string = header_string.replace("\n", " ").strip()
+
+    return header_string
+
+
+  def remove_annoying(self, val1, val2, expected, found):
+    """
+    Convenience function to remove items with XOR requirements
+
+    Keyword arguments:
+    val1 -- first XOR item
+    val2 -- seocnd XOR item
+    expected -- set of all required items
+    found -- set of items that were extracted from sheet
+    """
+
+    if ((val1 in expected and val1 in found) and
+      (val2 not in found)):
+      expected.remove(val2)
+    if ((val2 in expected and val2 in found) and
+      (val1 not in found)):
+      expected.remove(val1)
+
+    return expected
+
+
+  def check_headerRow(self, expected, found):
+    """
+    Check that a row of headers contains all the required values
+
+    Keyword arguments:
+    expected -- set of all required header values
+    found -- set of items that were extracted from sheet
+    """
+
+    # spreadsheets must have either a barcode field or a object ID field, but both are not required
+    header1 = 'barcode'
+    header2 = ('object identifier\n(edit heading to specify type' +
+      ' - e.g. barcode)')
+    expected = self.remove_annoying(header1, header2, expected, found)
+
+    missing = []
+
+    for header in expected:
+      if header not in found:
+        missing.append(header)
+
+    if missing:
+      self.raise_excelerror("Missing required value- {0}."
+        .format(missing_headers))
+
+    return True
+
+
+  def check_headerEntries(self, expected, found):
+    """
+    Check that a sheet contains the required heirarchy of headers
+
+    Keyword arguments:
+    expected -- set of all required header heirarchies values
+    found -- set of header heirarchies that were extracted from sheet
+    """
+
+    # spreadsheets must have either a barcode field or a object ID field, but both are not required
+    header1 = ('original master', 'object', 'barcode')
+    header2 = ('original master', 'object',
+      'object identifier\n(edit heading to specify type' +
+      '- e.g. barcode)'),
+    expected = self.remove_annoying(header1, header2, expected, found)
+
+    bad_entries = []
+
+    for header in expected:
+      if header not in found:
+        bad_entries.append(header)
+
+    if bad_entries:
+      self.raise_excelerror("Incorrect header entry for {0}."
+        .format(bad_entries))
+    return True
+
+
+  def check_noequations(self, sheetname):
+    """
+    Verify that no column in a sheet contains an equation
+    Based on checking every cell in the 4th row
+
+    Keyword arguments:
+    sheetname -- name of the sheet to normalize (self.pres_sheetname or
+    self.edit_sheetname)
+    """
+
+    self.wb_open = load_workbook(self.name, read_only = True)
+    sheet = self.wb_open.get_sheet_by_name(sheetname)
+
+    for i in range(1, self.wb.sheet_by_name(sheetname).ncols):
+      value = sheet.cell(row = 4, column = i).value
+      # equation check logic, might be better code out there
+      if (value and isinstance(value, str) and value[0] == "="):
+        self.raise_excelerror("Cell R4C{0} contain equations."
+          .format(i))
+    return True
+
+
+  def normalize_excelSheet(self, sheetname, conversion_dictionary):
+    """
+    Convert Excel sheet into pandas dataframe
+    Each list represents one row of the spreadsheets
+    Header values are normalized based on dictionary
+    Datetime values are automatically converted to ISO formats
+    Row values are normalized based on dictionary
+
+    Keyword arguments:
+    sheetname -- name of the sheet to normalize (self.pres_sheetname or
+    self.edit_sheetname)
+    conversion_dictionary -- dict where keys are values from
+    self.get_headerEntryAsString and values are normalized
+    dot-formatted strings
+    """
+
+    sheet = self.wb.sheet_by_name(sheetname)
+
+    ami_data = []
+
+    date_headers = ["bibliographic.date", "technical.dateCreated"]
+    time_headers = ["technical.durationHuman"]
+
+    #copy everything from the 3rd row to the last row with a filename
+    for rownum in range(2, sheet.nrows):
+      if sheet.cell(rownum, 0):
+        ami_data.append(sheet.row_values(rownum))
+
+    for i in range(0, sheet.ncols):
+      #normalize header
+      header_entry = self.get_headerEntryAsString(sheetname, i)
+      ami_data[0][i] = self.normalize_headerEntry(
+        header_entry,
+        conversion_dictionary)
+
+      #convert excel dates
+      if ami_data[0][i] in date_headers:
+        for j in range(3, sheet.nrows):
+          if sheet.cell(j, i).ctype == 3:
+            value = sheet.cell(j, i).value
+            ami_data[j-2][i] = self.convert_excelDateTime(value, "date")
+
+      #convert excel times
+      if ami_data[0][i] in time_headers:
+        for j in range(3, sheet.nrows):
+          if sheet.cell(j, i).ctype == 3:
+            value = sheet.cell(j, i).value
+            ami_data[j-2][i] = self.convert_excelDateTime(value, "time")
+
+    ami_df = self.normalize_values(ami_data)
+
+    return ami_df
+
+
+  def normalize_headerEntry(self, header_entry, conversion_dictionary):
+    """
+    Returns a normalized dot-formatted string based on the
+    heirarchy of headers from the first three rows of the Excel sheet.
+
+    Keyword arguments:
+    header_entry -- string of header values
+    conversion_dictionary -- dict where keys are values from
+    self.get_headerEntryAsString and values are normalized
+    dot-formatted strings
+    """
+
+    if header_entry not in conversion_dictionary.keys():
+      print(sheetname, column,
+        self.get_headerEntryAsTuple(sheetname, column),
+        header_entry)
+    return conversion_dictionary[header_entry]
+
+
+  def convert_excelDateTime(self, value, return_type):
+    """
+    Converts Excel's decimal encoded datetimes into ISO format strings.
+    Returns blank string if xldate conversion fails, otherwise, ISO
+    string.
+
+    Keyword arguments:
+    value -- decimal number encoding Excel datetimes
+    return_type -- (date, time) whether to return date or time
+    """
+
+    try:
+      converted_value = xldate.xldate_as_datetime(value,
+        self.wb.datemode)
+      if return_type == "date":
+        converted_value = converted_value.date().isoformat()
+      if return_type == "time":
+        converted_value = converted_value.time().isoformat()
+    except:
+      converted_value = ""
+
+    return converted_value
+
+
+  def normalize_values(self, data):
+    """
+    Normalize all entries via dictionaries defined in the ami_md_constants
+    module. Returns a list of lists
+
+    Keyword arguments:
+    data -- a list of lists where each list represents a row and the
+    first list contains headers
+    """
+
+    df = pd.DataFrame(data[1:], columns = data[0]).astype(str)
+
+    df = df.replace(ami_md_constants.NAS)
+
+    df = df.replace(ami_md_constants.REGEX_REPLACE_DICT, regex=True)
+    df = df.replace(ami_md_constants.STRING_REPLACE_DICT)
+    df['source.object.format_type'] = df['source.object.format'].map(ami_md_constants.FORMAT_TYPE)
+
+    for key in ami_md_constants.MEASURE_UNIT_MAPS.keys():
+      value_map = ami_md_constants.MEASURE_UNIT_MAPS[key]
+      df = self.map_value(df,
+        value_map['from_column'],
+        value_map['to_column'],
+        value_map['constant_value'],
+        value_map['values_map_column'],
+        value_map['values_map'])
+
+    #force all the numerics back to numeric, and drop all empty columns
+    df = df.apply(pd.to_numeric, errors='ignore').dropna(axis=1, how = "all")
+
+    vals = df.values.tolist()
+    cols = df.columns.tolist()
+    vals.insert(0, cols)
+
+    return vals
+
+
+  def map_value(self, df, from_column, to_column, value = None,
+    values_map_column = None, values_map = None):
+    """
+    Conditionally map units for columns with measures
+
+    Keyword arguments:
+    df -- pandas dataframe to operate on
+    from_column -- name of column to map from
+    to_column -- name of column to map new values to
+    value -- constant value to map into to_column
+    values_map_column -- name of column to determin variable mapping
+    values_map -- dictionary of mapping values
+    """
+    if from_column not in df.columns:
+      return df
+
+    if value:
+      df[to_column] = np.where(df[from_column].notnull(), value, None)
+    elif values_map_column and values_map:
+      #add unit value regardless if there is a measure value
+      df[to_column] = df[values_map_column].map(values_map)
+      #reset all unit values where there's no corresponding measure
+      df[to_column] = df[to_column].mask(df[from_column].isnull(), None)
+
+    return df
+
+
+
+  def write_amiCSV(self, sheetname, conversion_dictionary, csv_filename):
+    """
+    Convert a single Excel sheet into a CSV with normalized contents.
+
+    Keyword arguments:
+    sheetname -- name of the sheet to convert (self.pres_sheetname or
+    self.edit_sheetname)
+    conversion_dictionary -- dict where keys are values from
+    self.get_headerEntryAsString and values are normalized
+    dot-formatted strings
+    csv_filename -- path of the output file
+    """
+
+    ami_data = self.normalize_excelSheet(sheetname,
+      conversion_dictionary)
+
+    with open(csv_filename, 'w') as f:
+      cw = csv.writer(f, quoting = csv.QUOTE_ALL)
+      for rownum in range(0, len(ami_data)):
+        cw.writerow(ami_data[rownum])
+
+
+  def convert_amiExcelToJSON(self, sheetname, conversion_dictionary, json_directory):
+    """
+    Convert all rows in an Excel sheet into JSON files with
+    normalized data. Filename is based on described file's name.
+
+    Keyword arguments:
+    sheetname -- name of the sheet to convert (self.pres_sheetname or
+    self.edit_sheetname)
+    conversion_dictionary -- dict where keys are values from
+    self.get_headerEntryAsString and values are normalized
+    dot-formatted strings
+    json_directory -- path to output directory for json files
+    """
+
+    ami_data = self.normalize_excelSheet(sheetname,
+      conversion_dictionary)
+
+    cols = len(ami_data[0])
+
+    headers = ami_data[0]
+
+    json_directory = os.path.abspath(json_directory)
+
+    for row in ami_data[1:]:
+      tree = {}
+      for column in range(0, cols):
+        if row[column]:
+          tree = self.convert_dotKeyToNestedDict(tree,
+            headers[column], row[column])
+
+      json_filename = "{0}/{1}.{2}.json".format(
+        json_directory, tree['filename'],
+        tree['technical']['extension'])
+
+      with open(json_filename, 'w') as f:
+        json.dump(tree, f)
+
+
+  def convert_dotKeyToNestedDict(self, tree, key, value):
+    """
+    Recursive method that takes a dot-delimited header and returns a
+    nested dictionary.
+
+    Keyword arguments:
+    key -- dot-delimited header string
+    value -- value associated with header
+    """
+
+    t = tree
+    if "." in key:
+      key, rest = key.split(".", 1)
+      if key not in tree:
+        t[key] = {}
+      self.convert_dotKeyToNestedDict(t[key], rest, value)
+    else:
+      t[key] = value
+
+    return t
+
+
+  def raise_excelerror(self, msg):
+    """
+    lazy error reporting
+    """
+
+    raise AMIExcelError(msg)
+    logging.error(msg + '\n')
+    return False
