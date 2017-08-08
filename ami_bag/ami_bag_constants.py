af48e0117d32f86155a7e6450479c3093db225e7
nkrabben <n@nkrabben.com>
2017-08-08 15:50:37 -0400

    add directory/ext definitions

--- ami_bag/ami_bag_constants.py
+++ ami_bag/ami_bag_constants.py
@@ -1,5 +1,48 @@
 EXTS = [".mov", ".wav", ".mkv", ".iso", ".tar", ".mp4"]
 
-EXCEL_VIDEO_DIRS = ["Metadata", "PreservationMasters"]
-EXCEL_AUDIO_DIRS = EXCEL_VIDEO_DIRS[:]
-EXCEL_AUDIO_DIRS.append("EditMasters")
+
+MD_DIR = "Metadata"
+PM_DIR = "PreservationMasters"
+EM_DIR = "EditMasters"
+SC_DIR = "ServiceCopies"
+AO_DIR = "ArchiveOriginals"
+PF_DIR = "ProjectFiles"
+IM_DIR = "Images"
+
+MOV_EXT = ".mov"
+MKV_EXT = ".mkv"
+MP4_EXT = ".mp4"
+ISO_EXT = ".iso"
+TAR_EXT = ".tar"
+WAV_EXT = ".wav"
+EXCEL_EXT = ".xlsx"
+EDITEDEXCEL_EXT = ".old"
+JSON_EXT = ".json"
+JPEG_EXT = ".jpeg"
+
+EXCEL_SUBTYPES = {
+    "video": ([MD_DIR, PM_DIR],
+        [EXCEL_EXT, EDITEDEXCEL_EXT, MOV_EXT]),
+    "dvd": ([MD_DIR, PM_DIR],
+        [EXCEL_EXT, EDITEDEXCEL_EXT, ISO_EXT]),
+    "audio": ([MD_DIR, PM_DIR, EM_DIR],
+        [EXCEL_EXT, EDITEDEXCEL_EXT, WAV_EXT]),
+    "audio w/o edit masters": ([MD_DIR, PM_DIR],
+        [EXCEL_EXT, EDITEDEXCEL_EXT, WAV_EXT]),
+    "born-digital video": ([MD_DIR, AO_DIR, EM_DIR],
+        [EXCEL_EXT, EDITEDEXCEL_EXT, TAR_EXT, MOV_EXT]),
+    "born-digital audio": ([MD_DIR, AO_DIR, EM_DIR],
+        [EXCEL_EXT, EDITEDEXCEL_EXT, WAV_EXT])
+}
+
+EXCEL_SUBTYPES = {
+    "video": ([PM_DIR, SC_DIR, IM_DIR],
+        [JSON_EXT, MOV_EXT, MKV_EXT, MP4_EXT, JPEG_EXT]),
+    "audio": ([PM_DIR, SC_DIR, IM_DIR],
+        [JSON_EXT, WAV_EXT, JPEG_EXT])
+}
+
+EXCELJSON_SUBTYPES = {
+    "audio": ([MD_DIR, PM_DIR, SC_DIR, IM_DIR],
+        [EXCEL_EXT, EDITEDEXCEL_EXT, JSON_EXT, WAV_EXT, JPEG_EXT])
+}
