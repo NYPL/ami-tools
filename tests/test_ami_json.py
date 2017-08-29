2444426a3077050cb802a366a3cdb7a1c6b90ee3
nkrabben <n@nkrabben.com>
2017-08-29 17:21:27 -0400

    test json file load

--- tests/test_ami_json.py
+++ tests/test_ami_json.py
@@ -1,10 +1,19 @@
 import unittest
+import os
+
+from ami_md.ami_json import ami_json
+
+pm_json_dir = 'tests/test-data/json-video-bag/PreservationMasters'
+pm_json_filename = 'myd_263524_v01_pm.json'
+pm_json_path = os.path.join(pm_json_dir, pm_json_filename)
 
 class TestAMIJSON(unittest.TestCase):
 
-    def setUp(self):
-        self.tmpdir = tempfile.mkdtemp()
+  def test_load_json_file(self):
+    pm_json = ami_json(filepath = pm_json_path)
+    self.assertEqual(pm_json.filename, pm_json_filename)
+    self.assertTrue(hasattr(pm_json, 'dict'))
 
 
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()
