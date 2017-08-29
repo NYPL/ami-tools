7c8d11a56d7d17e58c61d971fb02b08502df0a57
nkrabben <n@nkrabben.com>
2017-08-29 17:33:27 -0400

    test for excepting non-json files, update namespace

--- tests/test_ami_json.py
+++ tests/test_ami_json.py
@@ -1,7 +1,7 @@
 import unittest
 import os
 
-from ami_md.ami_json import ami_json
+import ami_md.ami_json as aj
 
 pm_json_dir = 'tests/test-data/json-video-bag/PreservationMasters'
 pm_json_filename = 'myd_263524_v01_pm.json'
@@ -10,13 +10,19 @@ pm_json_path = os.path.join(pm_json_dir, pm_json_filename)
 class TestAMIJSON(unittest.TestCase):
 
   def test_load_json_file(self):
-    pm_json = ami_json(filepath = pm_json_path)
+    pm_json = aj.ami_json(filepath = pm_json_path)
     self.assertEqual(pm_json.filename, pm_json_filename)
     self.assertTrue(hasattr(pm_json, 'dict'))
+    self.assertTrue(pm_json.media_format_type == 'video')
 
   def test_dont_load_json_file(self):
-    pm_json = ami_json(filepath = pm_json_path, load = False)
+    pm_json = aj.ami_json(filepath = pm_json_path, load = False)
     self.assertFalse(hasattr(pm_json, 'dict'))
+
+  def test_load_bad_json_file(self):
+    not_json_path = pm_json_path.replace('json', 'mov')
+    self.assertRaises(aj.AMIJSONError, aj.ami_json,
+      filepath = not_json_path)
 
 if __name__ == '__main__':
   unittest.main()
