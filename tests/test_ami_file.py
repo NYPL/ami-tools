c87214820ee46068f327c51160d5318f02ff2ba5
nkrabben <n@nkrabben.com>
2017-09-06 10:17:12 -0400

    initiate ami_file class tests

--- tests/test_ami_file.py
+++ tests/test_ami_file.py
@@ -1,10 +1,24 @@
 import unittest
+import os
+
+import ami_files.ami_file as af
+
+pm_json_dir = 'tests/test-data/json-video-bag/PreservationMasters'
+pm_mov_filename = 'myd_263524_v01_pm.mov'
+pm_mov_path = os.path.join(pm_json_dir, pm_mov_filename)
 
 class TestAMIFile(unittest.TestCase):
 
-    def setUp(self):
-        self.tmpdir = tempfile.mkdtemp()
+  def test_load_media_file(self):
+    pm_file = af.ami_file(filepath = pm_mov_path)
+    self.assertEqual(pm_file.filename, pm_mov_filename)
+    self.assertTrue(hasattr(pm_file, 'base_filename'))
+    self.assertTrue(pm_file.type == 'video')
+
+  def test_dont_load_json_file(self):
+    self.assertRaises(af.AMIFileError, af.ami_file,
+      filepath = pm_mov_path.replace('2', '3'))
 
 
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()
