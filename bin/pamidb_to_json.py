2095adc57d991dcb8da3b4daf881e7288bf1921c
nkrabben <n@nkrabben.com>
2018-01-05 12:16:03 -0500

    clean up spacing on pamidb

--- bin/pamidb_to_json.py
+++ bin/pamidb_to_json.py
@@ -4,23 +4,23 @@ import ami_md.ami_json as aj
 
 
 def _make_parser():
-  parser = argparse.ArgumentParser()
-  parser.description = "convert a PAMIdb merge export to JSON records"
-  parser.add_argument("-i", "--mer", "--input",
-    help = "path to a PAMIdb merge export",
-    required = True)
-  parser.add_argument("-o", "--output",
-    help = "directory to save all json files",
-    required = True)
-  parser.add_argument("-s", "--schema",
-    help = "current schema version, preferred format x.y.z",
-    default = "2.0.0")
-  return parser
+	parser = argparse.ArgumentParser()
+	parser.description = "convert a PAMIdb merge export to JSON records"
+	parser.add_argument("-i", "--mer", "--input",
+		help = "path to a PAMIdb merge export",
+		required = True)
+	parser.add_argument("-o", "--output",
+		help = "directory to save all json files",
+		required = True)
+	parser.add_argument("-s", "--schema",
+		help = "current schema version, preferred format x.y.z",
+		default = "2.0.0")
+	return parser
 
 
 def main():
 	parser = _make_parser()
-  	args = parser.parse_args()
+		args = parser.parse_args()
 
 	md = pd.read_csv(args.input)
 	md = md.dropna(axis = 1, how = "all").astype(object)
@@ -35,4 +35,4 @@ def main():
 
 
 if __name__ == "__main__":
-  main()
+	main()
